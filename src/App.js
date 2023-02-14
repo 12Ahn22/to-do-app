e082506d9a30403968c64b8fe7e447b56d4e3507
12Ahn22 <44540726+12Ahn22@users.noreply.github.com>
2023-02-14 21:17:52 +0900

    Revert "Revert "release: 요구사항 완성""

--- src/App.js
+++ src/App.js
@@ -2,18 +2,16 @@ import { useState } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import TodoList from './components/TodoList/TodoList';
+import { DarkModeProvider } from './context/DarkModeContext';
 
 function App() {
-  const [todos, setTodos] = useState([
-    { id: '1', text: '공부하기', status: 'active' },
-    { id: '2', text: '청소하기', status: 'active' },
-  ]);
+  const [todos, setTodos] = useState([]);
 
   return (
-    <>
+    <DarkModeProvider>
       <Header todos={todos} />
       <TodoList setTodos={setTodos} todos={todos} />
-    </>
+    </DarkModeProvider>
   );
 }
 
--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,15 +1,19 @@
 import { TiDelete } from 'react-icons/ti';
 import { AiOutlineEdit } from 'react-icons/ai';
 import styles from './Todo.module.css';
+import { useState } from 'react';
 
 const Todo = ({ todo, onUpdate, onDelete }) => {
   const { text, status, id } = todo;
+  const [updatedText, setUpdatedText] = useState(text);
+  const [mode, setMode] = useState('read');
 
   const handleChange = (e) => {
     const status = e.target.checked ? 'completed' : 'active';
     onUpdate({
       ...todo,
       status,
+      text: updatedText,
     });
   };
 
@@ -17,20 +21,54 @@ const Todo = ({ todo, onUpdate, onDelete }) => {
     onDelete(todo);
   };
 
-  return (
-    <li className={styles.todo}>
-      <input
-        className={styles.checkbox}
-        type="checkbox"
-        id={id}
-        checked={status === 'completed'}
-        onChange={handleChange}
-      />
-      <label className={styles.text} htmlFor={id}>
-        {text}
+  const handleTextChange = (e) => {
+    setUpdatedText(e.target.value);
+    // onUpdate({
+    //   ...todo,
+    //   text: updatedText,
+    // });
+  };
+
+  const handleModeChange = (mode) => {
+    setMode(mode);
+  };
+
+  if (mode === 'write') {
+    return (
+      <li className={styles.todo}>
+        <input
+          className={`${styles.text} ${styles.input}`}
+          type="text"
+          value={updatedText}
+          onChange={handleTextChange}
+        />
+
+        <button
+          className={styles.button}
+          onClick={() => handleModeChange('read')}
+        >
+          <AiOutlineEdit />
+        </button>
+        <button className={styles.button} onClick={handleDelete}>
+          <TiDelete />
+        </button>
+      </li>
+    );
+  }
+
+      <label
+        className={`${styles.text} ${
+          status === 'completed' ? `${styles.completed}` : ''
+        }`}
+        htmlFor={id}
+      >
+        {updatedText}
       </label>
 
-      <button className={styles.button} onClick={onUpdate}>
+      <button
+        className={styles.button}
+        onClick={() => handleModeChange('write')}
+      >
         <AiOutlineEdit />
       </button>
       <button className={styles.button} onClick={handleDelete}>
