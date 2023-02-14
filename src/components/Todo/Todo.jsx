8d2893980065c5104b0f5cc6b7daccc35eec15d3
12ayo22 <12ayo22@gmail.com>
2023-02-14 20:08:25 +0900

    ✨ 작성된 Todo 편집기능 추가

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,21 +1,60 @@
 import { TiDelete } from 'react-icons/ti';
 import { AiOutlineEdit } from 'react-icons/ai';
 import styles from './Todo.module.css';
+import { useState } from 'react';
 
 const Todo = ({ todo, onUpdate, onDelete }) => {
   const { text, status, id } = todo;
+  const [updatedText, setUpdatedText] = useState(text);
+  const [mode, setMode] = useState('read');
 
   const handleChange = (e) => {
     const status = e.target.checked ? 'completed' : 'active';
     onUpdate({
       ...todo,
       status,
+      text: updatedText,
     });
   };
 
   const handleDelete = () => {
     onDelete(todo);
   };
+
+  const handleTextChange = (e) => {
+    setUpdatedText(e.target.value);
+    // onUpdate({
+    //   ...todo,
+    //   text: updatedText,
+    // });
+  };
+
+  const handleModeChange = (mode) => {
+    setMode(mode);
+  };
+
+  if (mode === 'write') {
+    return (
+      <li className={styles.todo}>
+        <input
+          className={`${styles.text} ${styles.input}`}
+          type="text"
+          value={updatedText}
+          onChange={handleTextChange}
+        />
+
+        <button
+          className={styles.button}
+          onClick={() => handleModeChange('read')}
+        >
+          <AiOutlineEdit />
+        </button>
+        <button className={styles.button} onClick={handleDelete}>
+          <TiDelete />
+        </button>
+      </li>
+    );
+  }
 
   return (
     <li className={styles.todo}>
@@ -32,10 +71,13 @@ const Todo = ({ todo, onUpdate, onDelete }) => {
         }`}
         htmlFor={id}
       >
-        {text}
+        {updatedText}
       </label>
 
-      <button className={styles.button} onClick={onUpdate}>
+      <button
+        className={styles.button}
+        onClick={() => handleModeChange('write')}
+      >
         <AiOutlineEdit />
       </button>
       <button className={styles.button} onClick={handleDelete}>
