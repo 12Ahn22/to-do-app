a90ec785f1392584b0369973f85d5d616c4c3ce1
12Ahn22 <12ayo22@gmail.com>
2023-02-22 13:44:15 +0900

    Feat: 내가 작업한 내용 커밋

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,7 +10,7 @@ const Header = ({ todos }) => {
 
   return (
     <header className={styles.header}>
-      테스트1 헤더 변경 ✨데브가 변경되었습니다.!
+      내가 작업한 feat 브랜치 내용
       <div>
         <span className={styles.count}>
           토탈: <span className={styles.accent}>{total}</span>
